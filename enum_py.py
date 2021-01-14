dec1c55daeb86654edaf493481dc1759e7d43e77
P Yagna narayanan <yagna2308@gmail.com>
2021-01-14 08:58:17 +0530

    Add files via upload

---
+++ enum_py.py
@@ -0,0 +1,5 @@
+list_1 = ["Ram", "Rahim", "Sam", "Peter"]
+# index starts with 0
+for index, value in enumerate(list_1):
+    if index % 2 == 0:
+        print(f"index is {index} and value is {value}")
