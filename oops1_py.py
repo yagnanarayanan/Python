d0c6c30af1d417999a008ee2dac59dae15f057ac
P Yagna narayanan <yagna2308@gmail.com>
2021-02-03 10:10:00 +0530

    Add files via upload

---
+++ oops1_py.py
@@ -0,0 +1,13 @@
+class Student:
+    pass
+
+
+yagna = Student()
+yagna.name = "Yagna Narayanan"
+yagna.std = 12
+yagna.section = "A"
+yagna.subjects = ["Physics", "Math", "Chemistry"]
+print(yagna.name)
+print(yagna.std)
+print(yagna.section)
+print(yagna.subjects[1])
