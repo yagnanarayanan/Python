e6aaea642f7af67d28536eae3159dc15c28e7180
P Yagna narayanan <yagna2308@gmail.com>
2021-02-18 10:49:39 +0530

    Add files via upload

---
+++ oops5_py.py
@@ -0,0 +1,30 @@
+class Employee:
+    no_of_leaves = 20
+
+    def __init__(self, name, salary, role):
+        self.name = name
+        self.salary = salary
+        self.role = role
+
+    def print_details(self):
+        return f"Name: {self.name}, Salary: {self.salary}, Role: {self.role}, Leaves: {self.no_of_leaves}"
+
+    @classmethod
+    def change_leaves(cls, new_leaves):
+        cls.no_of_leaves = new_leaves
+
+    @classmethod
+    def from_inst(cls, string):
+        # params = string.split("-")
+        # return cls(params[0], params[1], params[2])
+        return cls(*string.split("-"))
+
+
+yagna = Employee('Yagna Narayanan', 100, 'Learner')
+rohan = Employee('Rohan Sharma', 200, 'Instructor')
+karan = Employee.from_inst('Karan-480-Manager')
+print(rohan.print_details())
+print(yagna.print_details())
+print(karan.print_details())
+Employee.change_leaves(50)
+print(yagna.print_details())
